a5e32d7ea70c25b327464f9b076e8b3e42717898
KlingerSouza <souzaklinger54@gmail.com>
2025-02-15 21:56:21 -0400

    fix api

--- src/app.js
+++ src/app.js
@@ -2,32 +2,26 @@ import express from 'express';
 import routes from './routes/index';
 import { resolve } from 'node:path';
 import cors from 'cors';
+
 import './database';
 
 class App {
     constructor() {
         this.app = express();
 
-        this.configCors();
-        this.middlewares();
+        this.app.use(cors());
+        this.middlewwares();
         this.routes();
     }
 
-    configCors() {
-        this.app.use(cors({ origin: process.env.FRONTEND_URL || '*' }));
-    }
-
-    middlewares() {
+    middlewwares() {
         this.app.use(express.json());
-
         this.app.use(
-            '/product-file', 
-            express.static(resolve(__dirname, '..', 'uploads', 'products'))
+            '/product-file', express.static(resolve(__dirname, '..', 'uploads'))
         );
 
         this.app.use(
-            '/category-file', 
-            express.static(resolve(__dirname, '..', 'uploads', 'categories'))
+            '/category-file', express.static(resolve(__dirname, '..', 'uploads'))
         );
     }
 
